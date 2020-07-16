9b01756d319cf349bfce2c9f58021622e010516d
Alex <gusev.a.v.1979@eyandex.ru>
2020-07-16 11:20:08 +0300

    intermediate commit

--- src/library.js
+++ src/library.js
@@ -27,20 +27,12 @@ let elevation = {
 let azimuth = {
   startValue: -45.,
   endValue: 45.,
-
-
   [Symbol.iterator]() {
-    // ...it returns the iterator object:
-    // onward, for..of works only with that object,
-    // asking it for next values using next()
     return {
       start :  this.startValue,
       current: this.startValue,
       last: this.endValue,
-
-      // next() is called on each iteration by the for..of loop
-      next() { // (2)
-        // it should return the value as an object {done:.., value :...}
+      next() {
         if (this.current < this.last) {
           return { done: false, value: this.current = this.current+ (this.last - this.start)/60. };
         } else {
@@ -54,11 +46,7 @@ let azimuth = {
 let range = {
   startValue: 3.,
   endValue: 200000.,
-
   [Symbol.iterator]() {
-    // ...it returns the iterator object:
-    // onward, for..of works only with that object,
-    // asking it for next values using next()
     return {
       start :  this.startValue,
       current: this.startValue,
